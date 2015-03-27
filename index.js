d9cc85edc3c9cb9cf553209defca963f105dca84
Xander Dumaine <xander.dumaine@inin.com>
2015-03-26 23:33:45 -0400

    Add definitions for jingle message extensions

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var jingleStanza = require('jxt').createRegistry();
 
 
 jingleStanza.use(require('jxt-xmpp-types'));
-jingleStanza.use(require('./stanzas/error'));
 jingleStanza.use(require('./stanzas/iq'));
+jingleStanza.use(require('./stanzas/message'));
+jingleStanza.use(require('./stanzas/error'));
 jingleStanza.use(require('./stanzas/jingle'));
 jingleStanza.use(require('./stanzas/rtp'));
 jingleStanza.use(require('./stanzas/iceUdp'));
 jingleStanza.use(require('./stanzas/extdisco'));
+jingleStanza.use(require('./stanzas/jingleMessage'));
 
 module.exports = {
     getXml: function (data) {
